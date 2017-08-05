511cda6f1eb936ab5474fae4427690e06af6d638
danielmoralesp <daniel_moralesp@hotmail.com>
2017-08-04 20:11:43 -0500

    Add role to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  enum role: [ :user, :promoter, :admin ]
+
+  before_create :set_default_user
+
+  def set_default_user
+    self.role = :user
+  end
 end

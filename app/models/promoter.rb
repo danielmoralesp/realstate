c444787c508dd6fc79968cd98f0fa14c010b26b7
danielmoralesp <daniel_moralesp@hotmail.com>
2017-08-28 19:54:23 -0500

    Formulario de promotor funcionando

--- app/models/promoter.rb
+++ app/models/promoter.rb
@@ -1,6 +1,9 @@
 class Promoter < ApplicationRecord
   belongs_to :user
 
+  validates :user_id, :phone, :address, :description, presence: true
+  validates :phone, numericality: { only_integer: true }
+
   enum status: [ :inactive, :active ]
 
   before_create :set_default_promoter
@@ -8,4 +11,5 @@ class Promoter < ApplicationRecord
   def set_default_promoter
     self.status = :inactive
   end
+
 end
